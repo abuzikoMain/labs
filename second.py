2c5b8aeef9e32697c87b44c0d06476c7ad9c85fc
Anton Bazhenov <horikovich@yandex.ru>
2024-06-04 14:06:41 +0300

    Created two files second.py and test_second.py. Added two func's "defineDot"

---
+++ second.py
@@ -0,0 +1,15 @@
+
+def defineDotFirst(x, y):
+    x_line = 4
+    if x > x_line:
+        return "II"
+    else:
+        return "I"
+
+def defineDotSecond(x, y):
+    y_line = 4
+    if y > y_line:
+        return "I"
+    else:
+        return "II"
+
