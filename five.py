38c7fac985a7c391e57a91a9473b53811a0de5ac
Anton Bazhenov <horikovich@yandex.ru>
2024-06-05 15:08:02 +0300

    added 5 tasks to five.py

--- five.py
+++ five.py
@@ -1,1 +1,47 @@
+import math
+from random import randint
 
+# # Task 1
+# def task_1():
+#     ...
+
+# #### Вариант 1
+# #### Вариант 1
+# #### Вариант 1
+# #### Вариант 1
+# #### Вариант 1
+# #### Вариант 1
+
+# # Task 2
+# def task_2(x):
+#     return x ** 6 - 2 * x - 7
+# print(task_2(int(input("Task 2 x: "))))
+
+# # Task 3 
+# def task_3(x, y):
+#     f = math.sin(x + 1) - y - 1.2
+#     s = 2 * x + math.cos(y) - 2
+#     return (f, s)
+
+# print(task_3(float(input("Task 2 x: ")), float(input("Task 2 y: "))))
+
+# # Task 4
+# def remain_of_the_division(lst, element = 2, division = 0):
+#     return list(filter(lambda x: x % element == division, lst))
+
+# def task_4(first, second):
+#     first = len(remain_of_the_division(first))
+#     second = len(remain_of_the_division(second, division = 1))
+#     return first, second
+
+# print(task_4([randint(0,100) for each in range(50)], [randint(0,100) for each in range(50)]))
+
+# # Task 5
+# def task_5(nums):
+#     return min(nums), max(nums)
+
+# count = int(input("Count elements: "))
+# print(task_5([randint(0,100) for each in range(count)]))
+# del count
+
+# Task  6
