2745ea78f3f1903937d85e4aa6036554057b1716
Anton Bazhenov <horikovich@yandex.ru>
2024-06-06 16:31:05 +0300

    in seven lab input labs from eight. Must do refactor

--- seven.py
+++ seven.py
@@ -104,45 +104,121 @@ import re
 # print('Task 6')
 
 # print('a')
-# with open("seven.txt", "r") as file:
+# with open('seven.txt', 'r', encoding='utf-8') as file:
 #     max_length = max(len(line.strip()) for line in file)
-#     print("Длина самой длинной строки:", max_length)
+#     print('Длина самой длинной строки:', max_length)
 
 # print('б, в')
 # max_length_line_number = None
-# with open("seven.txt", "r") as file:
+# with open('seven.txt', 'r', encoding='utf-8') as file:
 #     lines = file.readlines()
 #     for index, line in enumerate(lines):
 #         if len(line.strip()) == max_length:
 #             max_length_line_number = index + 1  # Нумерация строк с 1
-#             print("Номер самой длинной строки:", max_length_line_number)
-#             print("Самая длинная строка:", line.strip())
+#             print('Номер самой длинной строки:', max_length_line_number)
+#             print('Самая длинная строка:', line.strip())
 #             break
 # del max_length, max_length_line_number
 
 # Task 7
 # print('Task 7')
 
-# with open("seven.txt", "r") as file:
+# with open('seven.txt', 'r', encoding='utf-8') as file:
 #     lines = file.readlines()
 
-# with open("output.txt", "w+") as file:
+# with open('output.txt', 'w+', encoding='utf-8') as file:
 #     file.writelines([f'{str(line.rstrip())[::-1]}\n' for line in lines])
 # del lines
 
 # Task 8
 # print('Task 8')
-# with open("seven.txt", "r") as file1, open("output.txt", "r") as file2:
+# with open('seven.txt', 'r', encoding='utf-8') as file1, open('output.txt', 'r', encoding='utf-8') as file2:
 #     line_number = 0
 #     for line1, line2 in zip(file1, file2):
 #         line_number += 1
 #         if line1 != line2:
-#             print(f"{line_number}")
+#             print(f'{line_number}')
 #             break
 #     else:
-#         print("Файлы идентичны.")
+#         print('Файлы идентичны.')
 # del line_number
 
 # Task 9
 # print('Task 9')
 
+# ---------------lab 8 Task 1 #
+def find_substr(subst, st):
+    if subst.lower() in st.lower():
+        return True
+    else:
+        return False
+    
+# ---------------lab 8 Task 2 #
+def first_last(let, st):
+    last_index = 0
+    first_index = 0
+    is_first_set = False
+    for index in range(len(st)):
+        if let == st[index] and not is_first_set:
+            first_index, last_index = index, index
+            is_first_set = True
+
+        if let == st[index] and index > last_index:
+            last_index = index
+    
+    return (first_index, last_index)
+# ----------------lab 8 Task 3 #
+from collections import Counter
+
+def most_common_top3(st):
+    st = st.split()
+    st = ''.join(st)
+    st = Counter(st)
+    st = st.most_common(3)
+    st = ''.join(f'{key} - {val}\n' for key, val in st)
+    return st
+print(most_common_top3('Python — высокоуровневый язык программирования общего назначения с динамической строгой типизацией и автоматическим управлением памятью'))
+
+# ----------------lab 8 Task 4 #
+import os.path
+
+def add_text_to_file(st):
+    if os.path.exists('MyFile.txt'):
+        with open('MyFile.txt', 'a', encoding='utf-8') as file:
+            file.write(st + '\n')
+        return 'Строка добавлена к существующим'
+    else:
+        with open('MyFile.txt', 'w+', encoding='utf-8') as file:
+            file.write(st + '\n')
+        return 'Файл был создан'
+
+# ----------------lab 8 Task 5 #
+
+def read_from_last(lines = 1, file_e = 'seven.txt'):
+    with open(file_e, 'r', encoding='utf-8') as file:
+        line = file.readlines()
+    if lines > len(line):
+        return f'В файле только {len(line)} строк'
+    lines = abs(lines)
+    return '\n'.join(line[len(line) - lines:])
+
+print(read_from_last(11))
+
+# ----------------lab 8 Task 6 #
+
+def camel(st):
+    func = lambda x: x[1].upper() if x[0] % 2 == 0 else x[1]
+    result = ''.join(map(func, enumerate(st)))
+    return result
+
+# ----------------lab 8 Task 7 #
+
+def remove_braces(st):
+    st = st.split()
+    st = ''.join(st)
+    st = Counter(st)
+    
+
+    return st
+print(remove_braces('Шила(лишнее (лишнее) лишнее) в мешке не утаишь (лишнее)'))
+add_text_to_file('Python — высокоуровневый язык программирования общего')
